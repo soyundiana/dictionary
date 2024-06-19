b5a82fd0686551b1b29efc314291c1f83375d2d9
soyundiana <142823980+soyundiana@users.noreply.github.com>
2024-06-19 22:44:06 +0100

    set up dictionary.js and .css (started using useState)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 
 import './App.css';
+import Dictionary from "./Dictionary.js";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-  Hello
+<main> 
+  <Dictionary/>
+  </main>
       </header>
+      <footer className="text-center"> Coded by Diana :-) </footer>
+      </div>
     </div>
   );
 }
 
-export default App;
